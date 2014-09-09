4ac98a00c49e81af6c28359344d3d4f5d09178fe
Renata Antunes <tataiantunes@hotmail.com>
2014-09-08 23:24:28 -0300

    Exercício 2 da Lista 1

--- Lista-1/Exercicio2.c
+++ Lista-1/Exercicio2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+// Sem ponteiros
+#include <stdio.h>
 
 int main (void)
 {
@@ -13,12 +15,42 @@ int main (void)
 
 int maior(int x, int y )
 {
+    int aux;
     if (x>y)
         printf("O %i eh maior que %i", x, y);
     else if (x==y)
         printf("O %i eh igual a %i", x, y);
     else
-        printf("O %i eh maior que %i", y,x);
+        aux=x;
+        x=y;
+        y=aux;
+        printf("O %i eh maior que %i", x,y);
+        
+    return 0;}
 
+//Com ponteiros
+#include <stdio.h>
+
+int trocar(int *a,int *b){
+    int aux;
+    aux=*a;
+    *a=*b;
+    *b=aux;
+    return 0;
+}
+int maior(int *a, int *b){
+    if (*a>*b){
+        printf("O maior eh %i e o segundo eh %i", *a,*b);}
+    else if(*b>*a){
+        trocar(a,b);
+        printf("O maior eh %i e o segundo eh %i", *a,*b);}
+    return 0;
+}
+int main(void)
+{
+    int a,b;
+    printf("Digite dois valores:\n");
+    scanf("%i\n%i", &a,&b);
+    maior(&a,&b);
     return 0;
 }
