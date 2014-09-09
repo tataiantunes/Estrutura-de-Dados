8fb9d7a5a1df1ebf141842f46ec708b2e518871d
Renata Antunes <tataiantunes@hotmail.com>
2014-09-08 22:52:13 -0300

    Update Exercicio1.c

--- Lista-1/Exercicio1.c
+++ Lista-1/Exercicio1.c
@@ -1,3 +1,5 @@
+// Basico
+
 #include <stdio.h>
 
 int main(void)
@@ -13,13 +15,34 @@ int main(void)
 
 int trocar(int x, int y)
 {
-    int z =0,w =0;
-    z=x;
-    w=y;
-    y=z;
-    x=w;
+    int aux;
+    aux=x;
+    x=y;
+    y=aux;
     printf("Invertendo, temos o primeiro numero %i e o segundo %i", x,y);
 
     return 0 ;
-    
 }
+
+//Usando Ponteiros
+
+#include <stdio.h>
+
+int trocar(int *a,int *b){
+    int aux;
+    aux=*a;
+    *a=*b;
+    *b=aux;
+    return 0;
+}
+
+int main(void){
+    int a,b;
+    printf("Digite dois numeros, A e B:\n");
+    scanf("%d\n%d", &a, &b);
+    printf("Os valores de A e B eram, respectivamente: %d e %d\n", a,b);
+    trocar(&a,&b);
+    printf("Agora, os valores de A e B são: %d e %d\n", a,b);
+    return 0;
+}
+
