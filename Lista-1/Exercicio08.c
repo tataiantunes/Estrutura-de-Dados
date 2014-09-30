55870f71e1aa5d2399bbbcce88fc2e3b986c5d07
Renata Antunes <tataiantunes@hotmail.com>
2014-09-30 16:54:20 -0300

    Exercício 8 da Lista 1

--- Lista-1/Exercicio08.c
+++ Lista-1/Exercicio08.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-  int
+  int a, i, maximo;
+  
+  void mm(int *v, int a, int *maximo);
+  
+  printf("Digite quantos elementos tera seu vetor:\n");
+  scanf("%i",&a);
+  
+  printf("Digite o valor dos seus elementos:\n");
+  for (i=0; i<a; i++)
+    {
+      scanf("%i", &v[i]);
+    }
+    
+  mm(v, a, &maximo, &ocorre);
+  
+  printf("O maximo eh: %i\n", maximo);
+  
+  printf("O maximo ocorre: %i\n", ocorre);
+  
+  return 0;
+}
 
+void mm(int *v, int a, int *maximo, int *ocorre);
+{
+    ocorre = 0;
+    int i, j, aux, count;
+    for(i=0; i < a; i++)
+    {
+        for(j= i + 1; j < a; j++)
+        {
+            if(v[i] > v[j])
+            {
+                aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+            }
+        }
+    }
+    *maximo = v[a-1];
+    
+    for (i=0; i < a; i++)
+      {
+        
+        count++;
+      }
+  
 
-  return (0);
 }
