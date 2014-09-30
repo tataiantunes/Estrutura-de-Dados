fbbe0b88b3a1012b60ae6c50d00e03d5c562fdda
Renata Antunes <tataiantunes@hotmail.com>
2014-09-30 10:58:06 -0300

    Exercício 6 da Lista 2

---
+++ Lista-2/Exercicio6.c
@@ -0,0 +1,15 @@
+/*
+6) Escreva uma função chamada troca que troca os valores dos parâmetros recebidos. Sua assinatura deve ser:
+void troca(float *a, float *b);
+*/
+
+#include<stdio.h>
+
+void troca(float *a, float *b)
+{
+  float aux;
+  *a = aux;
+  *a = *b;
+  *b = aux;
+
+} 
