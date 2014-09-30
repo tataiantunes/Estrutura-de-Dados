7d3e587788813e89570ed16233b63cb04e563e46
Renata Antunes <tataiantunes@hotmail.com>
2014-09-30 12:30:26 -0300

    Exercício 10 da Lista 2

---
+++ Lista-2/Exercicio10.c
@@ -0,0 +1,34 @@
+/*
+10) Escreva uma função que recebe uma string de caracteres e uma letra e devolve um vetor de inteiros contendo as posições 
+(índices no vetor da string) onde a letra foi encontrada e um inteiro contendo o tamanho do vetor criado 
+(total de letras iguais encontradas). 
+Utilize o retorno de um vetor para retornar os índices e um ponteiro para guardar o tamanho do vetor.
+*/
+
+#include<stdio.h>
+
+int main (void)
+{
+  char letra, str;
+  int tam, i;
+  
+  printf("Digite uma letra:\n");
+  scanf("%c\n", &letra);
+  printf("Qual sera o tamanho do seu vetor de strings?\n");
+  scanf("%i\n", &tam);
+  printf("Digite os caracteres que ira formar seu vetor:)
+  for (i=0, i<tam, i++)
+    scanf("%c", &str[i]);
+
+}
+
+void funcao ( char str )
+{
+  int v;
+
+
+
+  return ();  
+} 
+  
+  
