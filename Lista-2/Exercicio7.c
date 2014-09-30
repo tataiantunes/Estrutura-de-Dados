989069ffbe3ee77202dca33ddb8b0651b52be3a1
Renata Antunes <tataiantunes@hotmail.com>
2014-09-30 11:07:11 -0300

    Exercício 7 da Lista 2

---
+++ Lista-2/Exercicio7.c
@@ -0,0 +1,15 @@
+/*7)Crie uma função que receba uma string como parâmetro (de tamanho desconhecido) e retorne uma cópia da mesma. 
+A assinatura da função deve ser:
+char *strcopy(char *str);
+*/
+
+#include<stdio.h>
+
+char *strcopy (char *str)
+{
+  char copia;
+  *str = copia;
+  
+  return (copia);
+
+}
