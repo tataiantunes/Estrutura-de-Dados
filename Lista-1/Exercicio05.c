c5ee233b46a29ffd289e773f7028af6ae90fd123
Renata Antunes <tataiantunes@hotmail.com>
2014-10-02 09:09:42 -0300

    Exercício 5 da Lista 1

--- Lista-1/Exercicio05.c
+++ Lista-1/Exercicio05.c
@@ -1,3 +1,5 @@
+// Sem ponteiros:
+
 #include <stdio.h>
 
 int main()
@@ -8,6 +10,23 @@ int main()
     scanf("B: %f",&bb);
     soma = (aa+bb);
     aa=soma;
+    printf("A soma eh: %f. E A e B ficaram, respectivamente, %f e %f\n", soma,aa,bb);
+
+    return 0;
+}
+
+// Com ponteiros:
+
+#include <stdio.h>
+
+int main()
+{
+    float *aa,*bb,soma;
+    printf("Digite dois numeros:\n");
+    scanf("A: %f",&aa);
+    scanf("B: %f",&bb);
+    soma = (*aa+*bb);
+    *aa=soma;
     printf("A soma eh: %f. E A e B ficaram, respectivamente, %f e %f\n", soma,aa,bb);
 
     return 0;
