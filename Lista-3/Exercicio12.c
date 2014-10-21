0d24468f9e8b76519c4e3aef3bdc4959ea040527
Renata Antunes <tataiantunes@hotmail.com>
2014-10-21 10:06:55 -0300

    Exercício 12 da Lista 3

---
+++ Lista-3/Exercicio12.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdio.lib>
+
+int main(void)
+{
+  int **matriz;
+  int linha, coluna;
+  
+  printf("Digite o numero de linhas da matriz: ");
+  scanf("%i", &linha);
+  printf("Digite o numero de colunas da matriz: ");
+  scanf("%i", &coluna);
+  
+  matriz = (int**)malloc(linha*sizeof(int*));
+
+  int i,j;
+  
+  for (i = 0; i < linha; i++)
+  {
+    matriz[i] = (int*)malloc(coluna*sizeof(int));
+    for (j = 0; j < coluna; j++)
+      matriz[i][j] = 0;
+  }
+
+  for(i = 0; i < linha; i++)
+  {
+    for(j = 0; j < coluna; j++)
+    {
+      matriz[i][j] = rand() % 100;
+    }
+  }
+  
+  printf("\nMatriz:\n\n");
+  
+  for(i = 0; i < linha; i++) 
+  {
+    for(j = 0; j < coluna; j++)
+    {
+      printf("%i ",matriz[i][j]);
+    }
+  }
+
+  printf("\nMatriz transposta: ");
+  
+  for(j = 0; j < coluna; j++) 
+  {
+    for(i = 0; i < linha; i++)
+    {
+      printf("%i ",matriz[j][i]);
+    }
+  }
+
+  free(matriz);
+
+  return 0;
+  
+}
