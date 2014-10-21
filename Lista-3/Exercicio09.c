ce776a3f5ddcae6473e2df24f1225666954a67b0
Renata Antunes <tataiantunes@hotmail.com>
2014-10-21 09:21:32 -0300

    Exercício 9 da Lista 3

---
+++ Lista-3/Exercicio09.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdio.lib>
+
+int main(void)
+{
+  float **matriz;
+  int linha, coluna;
+  int i,j;
+  
+  printf("Digite o numero de linhas da matriz: ");
+  scanf("%i", &linha);
+  printf("Digite o numero de colunas da matriz: ");
+  scanf("%i", &coluna);
+  
+  matriz = (float*)malloc(coluna*sizeof(float));
+
+  for (i=0; i < linha; i++)
+  {
+    matriz[i] = (float*)malloc(linha*sizeof(float));
+  }
+  
+  for (i=0; i < linha; i++)
+  {
+    for (j=0; j < coluna; j++)
+    {
+      printf("Digite o valor da matriz[%i][%i]", i+1, j+1);
+      scanf("%f", &matriz[i][j]);
+    }
+  }
+  
+  printf("A matriz completa e organizada eh:\n");
+  
+  for (i=0; i < linha; i++)
+  {
+    for (j=0; j < coluna; j++)
+    {
+      printf("%.2f", matriz[i][j]);
+    }
+  }
+
+  return 0;
+}
